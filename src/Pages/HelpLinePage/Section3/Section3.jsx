bb34c234c5f9554ea63a763582433cf80b7a5494
humayonitbd <humayonforid44@gmail.com>
2023-09-12 13:43:42 +0600

    help page

--- src/Pages/HelpLinePage/Section3/Section3.jsx
+++ src/Pages/HelpLinePage/Section3/Section3.jsx
@@ -1,70 +1,75 @@
 import React from 'react';
+import { ArrowLongRightIcon } from '@heroicons/react/24/solid';
+
 
 const Section3 = () => {
     return (
-        <div className=' w-11/12  mx-auto py-20'>
+        <div className=' w-11/12  mx-auto pb-10'>
             <div className='grid grid-cols-3 gap-10'>
                 <div className='col-span-2'>
-                    <h1 className='text-5xl font-semibold mb-10'>Popular Questions</h1>
+                    <h1 className='text-5xl font-semibold mb-10'>Browse Help Center</h1>
                     <div className='grid grid-cols-2'>
                     <div className='mr-10'>
-                        <div className='flex justify-between items-center border py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                        <h3 className='text-xl font-semibold mb-5 link-hover '>What's New</h3>
+                        <div className='flex justify-between items-center  border-b py-4 px-2 mb-4'>
+                            <p className='mr-10 text-lg link-hover'>Updates to My Account</p>
+                            
                         </div>
-                        <div className='flex justify-between items-center border  py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                        <div className='flex justify-between items-center  border-b  py-4 px-2 mb-4'>
+                            <p className='mr-10 text-lg link-hover'>Getting Started With Calendars</p>
+                            
                         </div>
-                        <div className='flex justify-between items-center border  py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                        <div className='flex justify-between items-center  border-b  py-4 px-2 mb-4'>
+                            <p className='mr-10 text-lg link-hover'>Getting Started With Calendars</p>
+                            
                         </div>
-                        <div className='flex justify-between items-center border  py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                        <div className='flex justify-between items-center  border-b  py-4 px-2 mb-4'>
+                            <p className='mr-10 text-lg link-hover'>Getting Started With Calendars</p>
+                            
                         </div>
-                        <div className='flex justify-between items-center border  py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                        <div>
+                            <p className='font-bold text-lg link-hover'>See all <span><ArrowLongRightIcon className='w-6 h-6 ml-3 inline font-bold' /></span></p>
                         </div>
                         
                         
                     </div>
                     <div>
-                        <div className='flex justify-between items-center border  py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                    <h3 className='text-xl font-semibold mb-5 link-hover'>My Order</h3>
+                        <div className='flex justify-between items-center  border-b  py-4 px-2 mb-4'>
+                            <p className='mr-10 text-lg link-hover'>Updates to My Account</p>
+                            
                         </div>
-                        <div className='flex justify-between items-center border  py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                        <div className='flex justify-between items-center  border-b  py-4 px-2 mb-4'>
+                            <p className='mr-10 text-lg link-hover'>Getting Started With Calendars</p>
+                            
                         </div>
-                        <div className='flex justify-between items-center border  py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                        <div className='flex justify-between items-center  border-b  py-4 px-2 mb-4'>
+                            <p className='mr-10 text-lg link-hover'>Getting Started With Calendars</p>
+                            
                         </div>
-                        <div className='flex justify-between items-center border  py-4 px-2 mb-4'>
-                            <p className='mr-10 text-lg'>Getting Started With Calendars</p>
-                            <p><ChevronRightIcon className='h-4 w-4' /></p>
+                        <div className='flex justify-between items-center  border-b  py-4 px-2 mb-4'>
+                            <p className='mr-10 text-lg link-hover'>Getting Started With Calendars</p>
+                            
                         </div>
-                        
+                        <div>
+                            <p className='font-bold text-lg link-hover'>See all <span><ArrowLongRightIcon className='w-6 h-6 ml-3 inline font-bold' /></span></p>
+                        </div>
                         
                     </div>
                         
                     </div>
                 </div>
                 <div>
-                <h1 className='text-5xl font-bold mb-10'>Contact us</h1>
+                <h1 className='text-5xl font-bold mb-10'>Live Chat</h1>
 
                 <div>
-                    <h3 className='text-3xl font-semibold mb-5'>Phone</h3>
-                    <div className=''>
-                    <p className='text-lg'>Mon - Fri <span className='ml-14 '>7:00 AM - 3:00 AM EST</span></p>
-                    <p className='text-lg'>Sat - Sun <span className='ml-14 '>8:00 AM - 8:00 PM EST</span></p>
-                    </div>
-                    <p className='text-lg my-6'>Please fill out a call request. We will typically call you back in less than a minute.</p>
-                    <button className='btn inline bg-black text-white pt-6 pb-10 px-32 rounded-full'>Request Callback</button>
+                    <h3 className='text-3xl font-semibold mb-5'>Online</h3>
+                    <p className='text-lg my-6'>A Customer Care Specialist is ready to chat.</p>
+                    <button className='btn inline bg-black text-white pt-6 pb-10 px-32 rounded-full'>Start chart</button>
+                    <hr className='border-b my-10' />
+                    <h3 className='text-3xl font-semibold mb-5'>Email</h3>
+                    <p className='text-lg my-6'>Our VistaPrint Customer Care Team is here to help.</p>
+                    <button className='btn inline bg-black text-white pt-6 pb-10 px-32 rounded-full'>Send email</button>
                 </div>
                 </div>
             </div>
